bf66418540264b1884464421d7741e039d415b43
jsong073 <jaehasong073@gmail.com>
2020-04-14 13:23:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"#Variable Identification Assignment for Canvas" 
---
+++ src/VariableIdentification.java
@@ -0,0 +1,16 @@
+public class VariableIdentification {
+    public static void main (String[] args) {
+        String countryName = "South Korea";
+        double population = 51.64;
+        String countryCapital = "Seoul";
+        double currencyExchange = 1213.44;
+
+        System.out.println("Hey Mom and Dad.");
+        System.out.println("Here are some interesting details about the first country I'm visiting.");
+        System.out.println("Name: " + countryName);
+        System.out.println("Population Size: " + population + " million");
+        System.out.println("Capital: " + countryCapital);
+        System.out.println("Currency: One U.S. Dollar equals " + currencyExchange + " Korean Won.");
+        System.out.println("I'm so excited to visit Lotte World and ride the roller coasters!");
+    }
+}
